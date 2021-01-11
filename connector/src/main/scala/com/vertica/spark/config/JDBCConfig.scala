d9cfd4d484c971c5855dd965715db7a615d53bf5
Alex Rehnby <alexr@bitquilltech.com>
2021-01-11 13:22:00 -0800

    Small fix

--- connector/src/main/scala/com/vertica/spark/config/JDBCConfig.scala
+++ connector/src/main/scala/com/vertica/spark/config/JDBCConfig.scala
@@ -2,4 +2,4 @@ package com.vertica.spark.config
 
 import ch.qos.logback.classic.Level
 
-final case class JDBCConfig(host: String, port: Int, db: String, username: String, val password: String, override val logLevel: Level) extends GenericConfig
+final case class JDBCConfig(host: String, port: Int, db: String, username: String, password: String, override val logLevel: Level) extends GenericConfig
