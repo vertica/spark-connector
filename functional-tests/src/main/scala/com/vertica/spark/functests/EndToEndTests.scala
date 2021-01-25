c9775b11de390ece29f28bfff2c42c09cff254ec
Jonathan Louie <jonathanl@bitquilltech.com>
2021-01-25 10:49:28 -0800

    Add projection test

--- functional-tests/src/main/scala/com/vertica/spark/functests/EndToEndTests.scala
+++ functional-tests/src/main/scala/com/vertica/spark/functests/EndToEndTests.scala
@@ -1,11 +1,14 @@
 package com.vertica.spark.functests
 
+import java.sql.Connection
+
 import org.apache.spark.sql.types.{DoubleType, LongType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 
 class EndToEndTests(readOpts: Map[String, String], tablename: String, tablename20: String) extends AnyFlatSpec with BeforeAndAfterAll {
+  val conn: Connection = TestUtils.getJDBCConnection(readOpts("host"), db = readOpts("db"), user = readOpts("user"), password = readOpts("password"))
 
   private val spark = SparkSession.builder()
     .master("local[*]")
@@ -14,6 +17,7 @@ class EndToEndTests(readOpts: Map[String, String], tablename: String, tablename2
 
   override def afterAll(): Unit = {
     spark.close()
+    conn.close()
   }
 
   it should "read data from Vertica" in {
@@ -47,4 +51,33 @@ class EndToEndTests(readOpts: Map[String, String], tablename: String, tablename2
 
     assert(schema.toString equals sc.toString)
   }
+
+  it should "support data frame projection" in {
+    val tableName1 = "dftest1"
+    val stmt = conn.createStatement
+    val n = 3
+    TestUtils.createTableBySQL(conn, tableName1, "create table " + tableName1 + " (a int, b float)")
+    val insert = "insert into "+ tableName1 + " values(1, 2.2)"
+    TestUtils.populateTableBySQL(stmt, insert, n)
+
+    val df: DataFrame = spark.read.format("com.vertica.spark.datasource.VerticaSource").options(readOpts + ("tablename" -> tableName1)).load()
+
+    val filtered = df.select(df("a"))
+    val count = filtered.count()
+
+    assert(count == n)
+    assert (filtered.columns.mkString equals Array("a").mkString)
+  }
+
+  it should "support data frame filter" in {
+
+  }
+
+  it should "load data from Vertica table that is [SEGMENTED] on [ALL] nodes" in {
+
+  }
+
+
+
+
 }
--- functional-tests/src/main/scala/com/vertica/spark/functests/TestUtils.scala
+++ functional-tests/src/main/scala/com/vertica/spark/functests/TestUtils.scala
@@ -1,13 +1,48 @@
 package com.vertica.spark.functests
 
-import java.sql.Connection
+import java.sql.{Connection, DriverManager, Statement}
+import java.util.Properties
 
 object TestUtils {
+  def getJDBCConnection(host: String, port: Int = 5433, db: String, user: String, password: String): Connection = {
+    Class.forName("com.vertica.jdbc.Driver").newInstance()
+
+    val prop = new Properties()
+    prop.put("user", user)
+    prop.put("password", password)
+
+    getConnectionByProp(host, port, db, prop)(None)
+  }
+
+  def getJDBCUrl(host: String, port: Int = 5433, db: String): String = {
+    "jdbc:vertica://" + host + ":" + port + "/" + db
+  }
+
+  def getConnectionByProp(host: String, port: Int = 5433, db: String, prop: Properties)(overrideHost: Option[String]): Connection = {
+    Class.forName("com.vertica.jdbc.Driver").newInstance()
+
+    val h = overrideHost match {
+      case Some(m) => m
+      case None    => host
+    }
+    prop.setProperty("host", h)
+    val jdbcURI = getJDBCUrl(h, port, db)
+    DriverManager.getConnection(jdbcURI, prop)
+
+  }
+
   def createTableBySQL(conn: Connection, tableName: String, createTableStr: String): Boolean = {
     val stmt = conn.createStatement()
 
     stmt.execute("drop table if exists " + tableName)
     println(createTableStr)
     stmt.execute(createTableStr)
+  }
+
+
+  def populateTableBySQL(stmt: Statement, insertStr: String, numOfRows: Int) {
+    for (_ <- 0 until numOfRows) {
+      stmt.execute(insertStr)
+    }
   }
 }
