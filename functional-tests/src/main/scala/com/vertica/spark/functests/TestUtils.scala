d03465ec82d3f441ef71f91f7768b78bfbc2b2c8
Jonathan Louie <jonathanl@bitquilltech.com>
2021-01-25 16:51:19 -0800

    Port most tests from old connector

--- functional-tests/src/main/scala/com/vertica/spark/functests/TestUtils.scala
+++ functional-tests/src/main/scala/com/vertica/spark/functests/TestUtils.scala
@@ -3,6 +3,8 @@ package com.vertica.spark.functests
 import java.sql.{Connection, DriverManager, Statement}
 import java.util.Properties
 
+import org.apache.spark.sql.SparkSession
+
 object TestUtils {
   def getJDBCConnection(host: String, port: Int = 5433, db: String, user: String, password: String): Connection = {
     Class.forName("com.vertica.jdbc.Driver").newInstance()
@@ -39,10 +41,38 @@ object TestUtils {
     stmt.execute(createTableStr)
   }
 
+  def createTable(conn: Connection, tableName: String, isSegmented: Boolean = true, numOfRows: Int = 10): Unit = {
+    val stmt = conn.createStatement()
+    stmt.execute("drop table if exists " + tableName)
+    val createStr = "create table " + tableName + "(a int, b int) " +
+      (if (isSegmented) "segmented by hash(a)" else "unsegmented") + " all nodes;"
+    println(createStr)
+    stmt.execute(createStr)
+    populateTable(stmt, tableName, numOfRows)
+  }
+
+  private def populateTable(stmt: Statement, tableName: String, numOfRows: Int) {
+    for (i <- 0 until numOfRows) {
+      val insertStr = "insert into " + tableName + " values (" + i + " ," + i + ")"
+      println(insertStr);
+      stmt.execute(insertStr)
+    }
+  }
 
   def populateTableBySQL(stmt: Statement, insertStr: String, numOfRows: Int) {
     for (_ <- 0 until numOfRows) {
       stmt.execute(insertStr)
     }
+  }
+
+  def doCount(spark: SparkSession, opt:Map[String, String]):Long = {
+    val df = spark.read.format("com.vertica.spark.datasource.VerticaSource").options(opt).load()
+
+    df.cache()
+    df.show()
+    println("schema =" + df.schema)
+    val c = df.count()
+    println("count = " + c)
+    c
   }
 }
