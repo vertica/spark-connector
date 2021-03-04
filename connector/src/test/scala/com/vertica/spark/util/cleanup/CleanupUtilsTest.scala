9c39c194c61ef195d94d76775461d00380cd1cdb
Alex Rehnby <alexr@bitquilltech.com>
2021-03-04 12:19:24 -0800

    fix tests

--- connector/src/test/scala/com/vertica/spark/util/cleanup/CleanupUtilsTest.scala
+++ connector/src/test/scala/com/vertica/spark/util/cleanup/CleanupUtilsTest.scala
@@ -13,6 +13,8 @@
 
 package com.vertica.spark.util.cleanup
 
+import ch.qos.logback.classic.Level
+import com.vertica.spark.config.LogProvider
 import com.vertica.spark.datasource.fs.FileStoreLayerInterface
 import com.vertica.spark.util.error.ConnectorError
 import com.vertica.spark.util.error.ConnectorErrorType._
@@ -21,6 +23,8 @@ import org.scalatest.BeforeAndAfterAll
 import org.scalatest.flatspec.AnyFlatSpec
 
 class CleanupUtilsTest extends AnyFlatSpec with BeforeAndAfterAll with MockFactory with org.scalatest.OneInstancePerTest {
+
+  val cleanupUtils = new CleanupUtils(new LogProvider(Level.ERROR))
 
   it should "Cleans up a file with a single part" in {
     val filename = "file.parquet"
@@ -33,7 +37,7 @@ class CleanupUtilsTest extends AnyFlatSpec with BeforeAndAfterAll with MockFacto
 
     val fileCleanupInfo = FileCleanupInfo(filename, 0, 1)
 
-    CleanupUtils.checkAndCleanup(fileStoreLayer, fileCleanupInfo)
+    cleanupUtils.checkAndCleanup(fileStoreLayer, fileCleanupInfo)
   }
 
   it should "Don't perform any cleanup if other parts aren't complete" in {
@@ -47,7 +51,7 @@ class CleanupUtilsTest extends AnyFlatSpec with BeforeAndAfterAll with MockFacto
 
     val fileCleanupInfo = FileCleanupInfo(filename, 0, 2)
 
-    CleanupUtils.checkAndCleanup(fileStoreLayer, fileCleanupInfo)
+    cleanupUtils.checkAndCleanup(fileStoreLayer, fileCleanupInfo)
   }
 
   it should "Clean up a file with a multiple parts" in {
@@ -59,14 +63,14 @@ class CleanupUtilsTest extends AnyFlatSpec with BeforeAndAfterAll with MockFacto
     (fileStoreLayer.fileExists _).expects(filename+".cleanup1").returning(Right(false))
     (fileStoreLayer.fileExists _).expects(filename+".cleanup2").returning(Right(false))
 
-    CleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 0, 3))
+    cleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 0, 3))
 
     (fileStoreLayer.createFile _).expects(filename+".cleanup1").returning(Right(()))
     (fileStoreLayer.fileExists _).expects(filename+".cleanup0").returning(Right(true))
     (fileStoreLayer.fileExists _).expects(filename+".cleanup1").returning(Right(true))
     (fileStoreLayer.fileExists _).expects(filename+".cleanup2").returning(Right(false))
 
-    CleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 1, 3))
+    cleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 1, 3))
 
     (fileStoreLayer.createFile _).expects(filename+".cleanup2").returning(Right(()))
     (fileStoreLayer.fileExists _).expects(filename+".cleanup0").returning(Right(true))
@@ -77,7 +81,7 @@ class CleanupUtilsTest extends AnyFlatSpec with BeforeAndAfterAll with MockFacto
     (fileStoreLayer.removeFile _).expects(filename+".cleanup2").returning(Right(()))
     (fileStoreLayer.removeFile _).expects(filename).returning(Right(()))
 
-    CleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 2, 3))
+    cleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 2, 3))
   }
 
   it should "Pass on errors from the file store layer" in {
@@ -86,7 +90,7 @@ class CleanupUtilsTest extends AnyFlatSpec with BeforeAndAfterAll with MockFacto
     val fileStoreLayer = mock[FileStoreLayerInterface]
     (fileStoreLayer.createFile _).expects(filename+".cleanup0").returning(Left(ConnectorError(CreateFileError)))
 
-    CleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 0, 3)) match {
+    cleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 0, 3)) match {
       case Right(_) => ()
       case Left(err) => assert(err.err == CreateFileError)
     }
@@ -96,7 +100,7 @@ class CleanupUtilsTest extends AnyFlatSpec with BeforeAndAfterAll with MockFacto
     (fileStoreLayer.fileExists _).expects(filename+".cleanup1").returning(Right(true))
     (fileStoreLayer.fileExists _).expects(filename+".cleanup2").returning(Right(true))
 
-    CleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 1, 3)) match {
+    cleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 1, 3)) match {
       case Right(_) => ()
       case Left(err) => assert(err.err == CreateFileError)
     }
@@ -109,7 +113,7 @@ class CleanupUtilsTest extends AnyFlatSpec with BeforeAndAfterAll with MockFacto
     (fileStoreLayer.removeFile _).expects(filename+".cleanup1").returning(Left(ConnectorError(RemoveFileError)))
     (fileStoreLayer.removeFile _).expects(filename+".cleanup2").returning(Left(ConnectorError(RemoveFileError)))
 
-    CleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 2, 3)) match {
+    cleanupUtils.checkAndCleanup(fileStoreLayer, FileCleanupInfo(filename, 2, 3)) match {
       case Right(_) => ()
       case Left(err) => assert(err.err == RemoveFileError)
     }
