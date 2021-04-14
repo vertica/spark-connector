3dcad727a0f0c1fafa04925f73daf316ab5bbeda
Alex Rehnby <alexr@bitquilltech.com>
2021-04-13 18:30:46 -0700

    Add kerberos example project

---
+++ examples/kerberos-example/src/main/scala/example/Main.scala
@@ -0,0 +1,58 @@
+// (c) Copyright [2020-2021] Micro Focus or one of its affiliates.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package example
+
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.Config
+import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
+import org.apache.spark.sql.{DataFrame, Row, SaveMode, SparkSession}
+
+object Main extends App {
+  val conf: Config = ConfigFactory.load()
+
+  val opts = Map(
+    "host" -> conf.getString("functional-tests.host"),
+    "user" -> conf.getString("functional-tests.user"),
+    "db" -> conf.getString("functional-tests.db"),
+    "staging_fs_url" -> conf.getString("functional-tests.filepath"),
+    "kerberos_service_name" -> conf.getString("functional-tests.kerberos_service_name"),
+    "kerberos_host_name" -> conf.getString("functional-tests.kerberos_host_name"),
+    "jaas_config_name" -> conf.getString("functional-tests.jaas_config_name"),
+    "logging_level" -> {if(conf.getBoolean("functional-tests.log")) "DEBUG" else "OFF"}
+  )
+
+  val spark = SparkSession.builder()
+    .master("local[*]")
+    .appName("Vertica Connector Test Prototype")
+    .getOrCreate()
+
+  try {
+    val tableName = "dftest"
+    val schema = new StructType(Array(StructField("col1", IntegerType)))
+
+    val data = Seq(Row(77))
+    val df = spark.createDataFrame(spark.sparkContext.parallelize(data), schema).coalesce(1)
+    val mode = SaveMode.ErrorIfExists
+
+    df.write.format("com.vertica.spark.datasource.VerticaSource").options(opts + ("table" -> tableName)).mode(mode).save()
+    println("KERBEROS DEMO, WROTE TABLE")
+
+    val dfRead: DataFrame = spark.read.format("com.vertica.spark.datasource.VerticaSource").options(opts + ("table" -> tableName)).load()
+
+    dfRead.rdd.foreach(x => println("KERBEROS DEMO, READ: " + x))
+
+  } finally {
+    spark.close()
+  }
+}
