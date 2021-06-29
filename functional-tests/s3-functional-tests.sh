2a7f79c1896e56aea92f29ee1ab10a166d45e888
Alex Rehnby <alexr@bitquilltech.com>
2021-06-29 09:34:58 -0700

    Upgrade spark version in script

--- functional-tests/s3-functional-tests.sh
+++ functional-tests/s3-functional-tests.sh
@@ -1,9 +1,9 @@
 wget -P ../../ https://apache.mirror.colo-serv.net/spark/spark-3.0.3/spark-3.0.3-bin-without-hadoop.tgz
 wget -P ../../ https://archive.apache.org/dist/hadoop/common/hadoop-3.3.0/hadoop-3.3.0.tar.gz
 cd ../../
-tar xvf spark-3.0.2-bin-without-hadoop.tgz
+tar xvf spark-3.0.3-bin-without-hadoop.tgz
 tar xvf hadoop-3.3.0.tar.gz
-mv spark-3.0.2-bin-without-hadoop/ /opt/spark
+mv spark-3.0.3-bin-without-hadoop/ /opt/spark
 cd /opt/spark/conf
 mv spark-env.sh.template spark-env.sh
 export JAVA_HOME=/usr/lib/jvm/jre-11-openjdk
