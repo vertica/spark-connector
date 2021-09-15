5d12348c2a6317cf62440b689c9d286a6868505d
ravjotbrar <ravjotb@bitquilltech.com>
2021-09-14 17:27:06 -0700

    Change error for create external table option

--- connector/src/main/scala/com/vertica/spark/util/error/ErrorHandling.scala
+++ connector/src/main/scala/com/vertica/spark/util/error/ErrorHandling.scala
@@ -201,7 +201,7 @@ case class HostMissingError() extends ConnectorError {
 }
 case class InvalidCreateExternalTableOption() extends ConnectorError {
   override def getFullContext: String = "The 'create_external_table' param is invalid. Please specify " +
-    "'true' or 'false'."
+    "'new-data' or 'existing-data'."
 }
 case class DbMissingError() extends ConnectorError {
   def getFullContext: String = "The 'db' param is missing. Please specify the name of the Vertica " +
