d9544cc3dcbf4ebc99937f7e636a9a7e5d04ac87
Jonathan Louie <jonathanl@bitquilltech.com>
2021-09-20 16:40:44 -0700

    Add debug option for Kerberos example

--- examples/kerberos-example/run-kerberos-example.sh
+++ examples/kerberos-example/run-kerberos-example.sh
@@ -4,5 +4,8 @@ export SPARK_HOME=/opt/spark
 export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
 start-master.sh
 start-worker.sh spark://localhost:7077
-#export SPARK_SUBMIT_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5050
+if [ "$1" == "debug" ]
+  then
+    export SPARK_SUBMIT_OPTS=-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5050
+fi
 spark-submit --master spark://localhost:7077 --conf "spark.driver.extraClassPath={$SPARK_HOME}/conf/" --driver-java-options '-Djava.security.auth.login.config=/spark-connector/docker/client-krb/jaas.config' ./target/scala-2.12/spark-vertica-connector-kerberos-example-assembly-2.0.2.jar
