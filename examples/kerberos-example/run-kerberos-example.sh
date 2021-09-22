74a9b6d99bdf5acc7d039baa82be5437efe8e2c6
Alexey Temnikov <alexeyt@bitquilltech.com>
2021-09-22 11:55:04 -0700

    Fixes for HDFS to run/listen on HTTP and HTTPS ports

--- examples/kerberos-example/run-kerberos-example.sh
+++ examples/kerberos-example/run-kerberos-example.sh
@@ -1,5 +1,5 @@
 export JAVA_HOME=/usr/lib/jvm/jre-11-openjdk
-export SPARK_DIST_CLASSPATH=$(/hadoop-3.3.0/bin/hadoop classpath)
+export SPARK_DIST_CLASSPATH=$(/hadoop-3.3.1/bin/hadoop classpath)
 export SPARK_HOME=/opt/spark
 export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
 start-master.sh
