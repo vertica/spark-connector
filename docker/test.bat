61313bddb9556cbdc8313285fedf6656a16c591d
Alexey Temnikov <alexeyt@bitquilltech.com>
2021-09-22 14:37:13 -0700

    Delete test.bat
    Not needed in this PR

--- docker/test.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-
-FOR /F "tokens=* USEBACKQ" %%F IN (`docker inspect -f "{{with index .NetworkSettings.Networks \"EXAMPLE.COM\"}}{{.IPAddress}}{{end}}" hdfs`) DO (
-	SET ip_address_var=%%F
-)
-ECHO %ip_address_var%
-
