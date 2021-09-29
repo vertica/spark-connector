766d510cb8914c77dc6c3ae4275fa7c08d495509
ravjotbrar <ravjotb@bitquilltech.com>
2021-09-28 17:45:00 -0700

    update version to 2to 2.0.3

--- examples/kerberos-example/build.sbt
+++ examples/kerberos-example/build.sbt
@@ -14,7 +14,7 @@
 scalaVersion := "2.12.12"
 name := "spark-vertica-connector-kerberos-example"
 organization := "com.vertica"
-version := "2.0.2"
+version := "2.0.3"
 
 resolvers += "Artima Maven Repository" at "https://repo.artima.com/releases"
 resolvers += "jitpack" at "https://jitpack.io"
--- examples/run-example.sh
+++ examples/run-example.sh
@@ -4,5 +4,5 @@ export SPARK_HOME=/opt/spark
 export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
 start-master.sh
 start-worker.sh spark://localhost:7077
-spark-submit --master spark://localhost:7077 $1/target/scala-2.12/spark-vertica-connector-$1-assembly-2.0.2.jar
+spark-submit --master spark://localhost:7077 $1/target/scala-2.12/spark-vertica-connector-$1-assembly-2.0.3.jar
 
