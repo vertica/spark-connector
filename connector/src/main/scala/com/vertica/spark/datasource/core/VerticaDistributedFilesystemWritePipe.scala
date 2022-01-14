79cc4810c5a312b71c55a5a145dfd557de3bf7e3
jeremyp-bq <94406158+jeremyp-bq@users.noreply.github.com>
2022-01-14 11:41:12 -0800

    Do not create a job status table for external tables (#309)
    * Do not create a job status table for external tables (#290)
    
    * Format long line (#290)

--- connector/src/main/scala/com/vertica/spark/datasource/core/VerticaDistributedFilesystemWritePipe.scala
+++ connector/src/main/scala/com/vertica/spark/datasource/core/VerticaDistributedFilesystemWritePipe.scala
@@ -150,7 +150,11 @@ class VerticaDistributedFilesystemWritePipe(val config: DistributedFilesystemWri
       _ <- if(existingData) Right(()) else fileStoreLayer.createDir(getAddress(), perm.toString)
 
       // Create job status table / entry
-      _ <- tableUtils.createAndInitJobStatusTable(config.tablename, config.jdbcConfig.auth.user, config.sessionId, if(config.isOverwrite) "OVERWRITE" else "APPEND")
+      _ <- if(config.createExternalTable.isDefined) {
+        Right(())
+      } else {
+        tableUtils.createAndInitJobStatusTable(config.tablename, config.jdbcConfig.auth.user, config.sessionId, if(config.isOverwrite) "OVERWRITE" else "APPEND")
+      }
     } yield ()
   }
 
@@ -494,8 +498,6 @@ class VerticaDistributedFilesystemWritePipe(val config: DistributedFilesystemWri
               )
 
       _ <- tableUtils.validateExternalTable(config.tablename)
-
-      _ <- tableUtils.updateJobStatusTable(config.tablename, config.jdbcConfig.auth.user, 0.0, config.sessionId, true)
 
     } yield ()
 
