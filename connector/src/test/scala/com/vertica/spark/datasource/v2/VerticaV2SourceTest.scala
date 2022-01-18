4f7be5d4a3e10a00c88cbb7f3fac1d469ed58cdc
Alex Le <35547559+Aryex@users.noreply.github.com>
2022-01-18 14:09:41 -0800

    minor cleanup

--- connector/src/test/scala/com/vertica/spark/datasource/v2/VerticaV2SourceTest.scala
+++ connector/src/test/scala/com/vertica/spark/datasource/v2/VerticaV2SourceTest.scala
@@ -77,8 +77,7 @@ class VerticaV2SourceTests extends AnyFlatSpec with BeforeAndAfterAll with MockF
     sessionId = "id",
     failedRowPercentTolerance =  0.0f,
     filePermissions = ValidFilePermissions("777").getOrElse(throw new Exception("File perm error")),
-    createExternalTable = None,
-    saveMetadataTables = false
+    createExternalTable = None
   )
 
   val intSchema = new StructType(Array(StructField("col1", IntegerType)))
