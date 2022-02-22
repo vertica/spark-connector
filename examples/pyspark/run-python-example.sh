5690ec90ca16a4bca77353986ad732689478c2d2
Alex Le <35547559+Aryex@users.noreply.github.com>
2022-02-22 09:06:30 -0800

    Updated version number (#333)
    * updated version number
    
    * Removed code.
    
    * Removed code.
    
    * Fixed github main.yml
    
    * Fixed CONTRIBUTING.md

--- examples/pyspark/run-python-example.sh
+++ examples/pyspark/run-python-example.sh
@@ -5,7 +5,7 @@ export SPARK_HOME=/opt/spark
 export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
 start-master.sh
 start-worker.sh spark://localhost:7077
-spark-submit --jars ../../connector/target/scala-2.12/spark-vertica-connector-assembly-3.0.2.jar sparkapp.py
+spark-submit --jars ../../connector/target/scala-2.12/spark-vertica-connector-assembly-3.0.3.jar sparkapp.py
 
 
 
