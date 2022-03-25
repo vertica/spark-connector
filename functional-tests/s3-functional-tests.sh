515cc3c2d7a0c358135ebb46903988fe36188f34
AlexL <quang1187580@gmail.com>
2022-03-25 12:19:01 -0700

    updated version to 3.1.0

--- functional-tests/s3-functional-tests.sh
+++ functional-tests/s3-functional-tests.sh
@@ -4,4 +4,4 @@ export SPARK_HOME=/opt/spark
 export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
 start-master.sh
 start-slave.sh spark://localhost:7077
-spark-submit --master spark://localhost:7077 target/scala-2.12/spark-vertica-connector-functional-tests-assembly-3.0.4.jar
+spark-submit --master spark://localhost:7077 target/scala-2.12/spark-vertica-connector-functional-tests-assembly-3.1.0.jar
