0a8192d6b1d6e7e5dafe274bfd743cea208eacca
AlexL <quang1187580@gmail.com>
2022-04-06 10:01:27 -0700

    Parametrized sandbox-clientenv.sh

--- docker/sandbox-clientenv.sh
+++ docker/sandbox-clientenv.sh
@@ -33,7 +33,15 @@ function configure_containers() {
   configure_db
 }
 
-if [ "$1" == "kerberos" ]
+while getopts 'kv:' c
+do
+  case $c in
+    k) KERBEROS=1 ;;
+    v) export VERTICA_VERSION=$OPTARG ;;
+  esac
+done
+
+if [ $KERBEROS == 1 ]
   then
     echo "running kerberos docker compose"
     docker compose -f docker-compose-kerberos.yml up -d
@@ -44,13 +52,13 @@ else
   docker compose -f docker-compose.yml up -d
   docker exec docker_vertica_1 /bin/sh -c "opt/vertica/bin/admintools -t create_db --database=docker --password='' --hosts=localhost"
   docker exec docker_vertica_1 /bin/sh -c "sudo /usr/sbin/sshd -D"
-#  docker exec docker_client_1 /bin/sh -c "cp /etc/hadoop/conf/* /hadoop-3.3.0/etc/hadoop/"
-#  docker exec docker_hdfs_1 cp /hadoop/conf/core-site.xml /opt/hadoop/etc/hadoop/core-site.xml
-#  docker exec docker_hdfs_1 cp /hadoop/conf/hdfs-site.xml /opt/hadoop/etc/hadoop/hdfs-site.xml
-#  docker exec docker_hdfs_1 /opt/hadoop/sbin/stop-dfs.sh
-#  docker exec docker_hdfs_1 /opt/hadoop/sbin/start-dfs.sh
-#  docker cp ../functional-tests/src/main/resources/3.1.1 docker_hdfs_1:/partitioned
-#  docker exec docker_hdfs_1 hadoop fs -copyFromLocal /partitioned /3.1.1
-#  docker exec docker_vertica_1 vsql -c "ALTER DATABASE docker SET MaxClientSessions=100;"
-#  docker exec -it docker_client_1 /bin/bash
+  docker exec docker_client_1 /bin/sh -c "cp /etc/hadoop/conf/* /hadoop-3.3.0/etc/hadoop/"
+  docker exec docker_hdfs_1 cp /hadoop/conf/core-site.xml /opt/hadoop/etc/hadoop/core-site.xml
+  docker exec docker_hdfs_1 cp /hadoop/conf/hdfs-site.xml /opt/hadoop/etc/hadoop/hdfs-site.xml
+  docker exec docker_hdfs_1 /opt/hadoop/sbin/stop-dfs.sh
+  docker exec docker_hdfs_1 /opt/hadoop/sbin/start-dfs.sh
+  docker cp ../functional-tests/src/main/resources/3.1.1 docker_hdfs_1:/partitioned
+  docker exec docker_hdfs_1 hadoop fs -copyFromLocal /partitioned /3.1.1
+  docker exec docker_vertica_1 vsql -c "ALTER DATABASE docker SET MaxClientSessions=100;"
+  docker exec -it docker_client_1 /bin/bash
 fi
