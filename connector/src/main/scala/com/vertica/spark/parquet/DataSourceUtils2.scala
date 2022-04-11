35cb20a08bc7bf02eb52e8d8ebbf4e7a9ffe344c
Alex Le <35547559+Aryex@users.noreply.github.com>
2022-04-11 15:17:05 -0700

    Added code copied from spark 3.2.0

---
+++ connector/src/main/scala/com/vertica/spark/parquet/DataSourceUtils2.scala
@@ -0,0 +1,32 @@
+package org.apache.spark.sql.execution.datasources
+
+import org.apache.spark.sql.catalyst.util.RebaseDateTime
+import org.apache.spark.sql.internal.SQLConf.LegacyBehaviorPolicy
+
+object DataSourceUtils2 {
+  // DONT DELETE
+  def creteDateRebaseFuncInRead(
+                                 rebaseMode: LegacyBehaviorPolicy.Value,
+                                 format: String): Int => Int = rebaseMode match {
+    case LegacyBehaviorPolicy.EXCEPTION => days: Int =>
+      if (days < RebaseDateTime.lastSwitchJulianDay) {
+        throw DataSourceUtils.newRebaseExceptionInRead(format)
+      }
+      days
+    case LegacyBehaviorPolicy.LEGACY => RebaseDateTime.rebaseJulianToGregorianDays
+    case LegacyBehaviorPolicy.CORRECTED => identity[Int]
+  }
+
+  // DONT DELETE
+  def creteTimestampRebaseFuncInRead(
+                                      rebaseMode: LegacyBehaviorPolicy.Value,
+                                      format: String): Long => Long = rebaseMode match {
+    case LegacyBehaviorPolicy.EXCEPTION => micros: Long =>
+      if (micros < RebaseDateTime.lastSwitchJulianTs) {
+        throw DataSourceUtils.newRebaseExceptionInRead(format)
+      }
+      micros
+    case LegacyBehaviorPolicy.LEGACY => RebaseDateTime.rebaseJulianToGregorianMicros
+    case LegacyBehaviorPolicy.CORRECTED => identity[Long]
+  }
+}
--- connector/src/main/scala/com/vertica/spark/parquet/ParquetRowConverter.scala
+++ connector/src/main/scala/com/vertica/spark/parquet/ParquetRowConverter.scala
@@ -21,21 +21,19 @@ package org.apache.spark.sql.execution.datasources.parquet.vertica
 import java.math.{BigDecimal, BigInteger}
 import java.nio.ByteOrder
 import java.time.{ZoneId, ZoneOffset}
-
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
-
 import org.apache.parquet.column.Dictionary
 import org.apache.parquet.io.api.{Binary, Converter, GroupConverter, PrimitiveConverter}
 import org.apache.parquet.schema.{GroupType, OriginalType, Type}
 import org.apache.parquet.schema.OriginalType.LIST
 import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.{BINARY, FIXED_LEN_BYTE_ARRAY, INT32, INT64, INT96}
-
 import org.apache.spark.internal.Logging
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions._
+import org.apache.spark.sql.catalyst.util.RebaseDateTime.RebaseSpec
 import org.apache.spark.sql.catalyst.util.{ArrayBasedMapData, CaseInsensitiveMap, DateTimeUtils, GenericArrayData}
-import org.apache.spark.sql.execution.datasources.DataSourceUtils
+import org.apache.spark.sql.execution.datasources.{DataSourceUtils, DataSourceUtils2}
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.internal.SQLConf.LegacyBehaviorPolicy
 import org.apache.spark.sql.types._
@@ -189,15 +187,27 @@ private[parquet] class ParquetRowConverter(
    */
   def currentRecord: InternalRow = currentRow
 
-  private val dateRebaseFunc = DataSourceUtils.creteDateRebaseFuncInRead(
-    datetimeRebaseMode, "Parquet")
+  // Pushdown of aggregates added in spark 3.2, detect spark version so we return class with the correct feature support
+  var sparkNewerThan320 = false
+  // try {
+  //   val sparkVersion = SparkSession.getActiveSession.get.version
+  //   val versionList = sparkVersion.split("\\.").map(_.toInt)
+  //   if(versionList.length >= 2 && versionList(0) == 3 && versionList(1) == 2 && versionList(2) < 1) {
+  //     sparkNewerThan320 = false
+  //   }
+  // }
+  // catch { // Couldn't recgonize version string, assume newer version
+  //   case _: java.lang.NumberFormatException => sparkNewerThan320 = true
+  // }
 
-  private val timestampRebaseFunc = DataSourceUtils.creteTimestampRebaseFuncInRead(
-    datetimeRebaseMode, "Parquet")
+  private val dateRebaseFunc = if(sparkNewerThan320) DataSourceUtils.createDateRebaseFuncInRead(datetimeRebaseMode, "Parquet")
+  else DataSourceUtils2.creteDateRebaseFuncInRead(datetimeRebaseMode, "Parquet")
 
-  private val int96RebaseFunc = DataSourceUtils.creteTimestampRebaseFuncInRead(
-    int96RebaseMode, "Parquet INT96")
+  private val timestampRebaseFunc: Long => Long = if(sparkNewerThan320) DataSourceUtils.createTimestampRebaseFuncInRead(RebaseSpec(datetimeRebaseMode), "Parquet")
+  else DataSourceUtils2.creteTimestampRebaseFuncInRead(datetimeRebaseMode, "Parquet")
 
+  private val int96RebaseFunc = if(sparkNewerThan320) DataSourceUtils.createTimestampRebaseFuncInRead(RebaseSpec(int96RebaseMode), "Parquet INT96")
+  else DataSourceUtils2.creteTimestampRebaseFuncInRead(int96RebaseMode, "Parquet INT96")
   // Converters for each field.
   private[this] val fieldConverters: Array[Converter with HasParentContainerUpdater] = {
     // (SPARK-31116) Use case insensitive map if spark.sql.caseSensitive is false
