f9b40569f2015aa46978a554d40857f6a6df102b
Alex Le <35547559+Aryex@users.noreply.github.com>
2022-04-12 13:39:47 -0700

    Added comments

--- connector/src/main/scala/com/vertica/spark/parquet/VerticaDataSourceUtils.scala
+++ connector/src/main/scala/com/vertica/spark/parquet/VerticaDataSourceUtils.scala
@@ -1,14 +1,18 @@
-package com.vertica.spark.parquet.vertica
+package org.apache.spark.sql.execution.datasources.parquet.vertica
 
 import org.apache.spark.sql.catalyst.util.RebaseDateTime
 import org.apache.spark.sql.execution.datasources.DataSourceUtils
 import org.apache.spark.sql.internal.SQLConf.LegacyBehaviorPolicy
 
 /**
+ * Copied from Spark 3.2.0 DataSourceUtils implementation.
  *
+ * Classes under the parquet package were copied from Spark for reading parquet into Spark. It is not a public API and
+ * thus is expected to change. In Spark 3.2.1, the DataSourceUtils interface was changed. To fix this, this class
+ * copied from Spark 3.2.0 only the needed functions.
  * */
 object VerticaDataSourceUtils {
-  // DONT DELETE
+
   def createDateRebaseFuncInRead(
                                  rebaseMode: LegacyBehaviorPolicy.Value,
                                  format: String): Int => Int = rebaseMode match {
@@ -21,7 +25,6 @@ object VerticaDataSourceUtils {
     case LegacyBehaviorPolicy.CORRECTED => identity[Int]
   }
 
-  // DONT DELETE
   def createTimestampRebaseFuncInRead(
                                       rebaseMode: LegacyBehaviorPolicy.Value,
                                       format: String): Long => Long = rebaseMode match {
