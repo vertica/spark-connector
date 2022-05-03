cdc1cc3f6de41b641e9f3c31da97788438b9867d
Alex Le <35547559+Aryex@users.noreply.github.com>
2022-05-02 17:33:23 -0700

    Added GCS example

---
+++ examples/gcs-example/README.md
@@ -0,0 +1,7 @@
+## Google Cloud Storage Example
+
+To start, follow the instruction [here](https://github.com/vertica/spark-connector/blob/main/GCSUserManual.md)
+to obtain the necessary credentials, then edit the appropriate configurations in `./src/main/resources/application.conf`.
+
+The example will use GCS as the intermediary area to write a simple table to Vertica before read it back.
+
---
+++ examples/gcs-example/src/main/scala/example/Main.scala
@@ -0,0 +1,71 @@
+// (c) Copyright [2020-2021] Micro Focus or one of its affiliates.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package example
+
+import com.typesafe.config.ConfigFactory
+import com.typesafe.config.Config
+import org.apache.spark.sql.types.{IntegerType, StructField, StructType}
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val conf: Config = ConfigFactory.load()
+    // Configuration options for the connector
+    val options = Map(
+      "host" -> conf.getString("app.host"),
+      "db" -> conf.getString("app.db"),
+      "user" -> conf.getString("app.db_user"),
+      "password" -> conf.getString("app.db_password"),
+      // Path to your service account keyfile
+      "gcs_keyfile" -> conf.getString("app.keyfile_path"),
+      // Your service account HMAC key
+      "gcs_hmac_key_id" -> conf.getString("app.hmac_key_id"),
+      "gcs_hmac_key_secret" -> conf.getString("app.hmac_key_secret"),
+      // Your GCS bucket address
+      "staging_fs_url" -> conf.getString("app.gcs_bucket"),
+    )
+
+    // Initialize Spark
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("Vertica Connector Test Prototype")
+      .getOrCreate()
+
+    val verticaSource = "com.vertica.spark.datasource.VerticaSource"
+    try {
+      val tableName = "dftest"
+      val rdd = spark.sparkContext.parallelize(Seq(
+        Row(23),
+        Row(35),
+        Row(75),
+        Row(96)
+      )).coalesce(1)
+      val schema = StructType(Array(StructField("col1", IntegerType)))
+      val writeOpts = options + ("table" -> tableName)
+      spark.createDataFrame(rdd, schema)
+        .write.format(verticaSource)
+        .options(writeOpts)
+        .mode(SaveMode.Overwrite)
+        .save()
+
+      val readOpts = options + ("table" -> tableName)
+      spark.read.format(verticaSource)
+        .options(readOpts)
+        .load()
+        .show()
+    } finally {
+      spark.close()
+    }
+  }
+}
