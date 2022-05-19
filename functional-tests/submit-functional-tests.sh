ebc3a95091e049c460c976b4f48bd1c54fe44237
Alex Le <35547559+Aryex@users.noreply.github.com>
2022-05-19 11:27:25 -0700

    Edit docker ports to expose Spark driver web UI

--- functional-tests/submit-functional-tests.sh
+++ functional-tests/submit-functional-tests.sh
@@ -4,5 +4,5 @@ export SPARK_DIST_CLASSPATH=$(/hadoop-3.3.0/bin/hadoop classpath)
 export SPARK_HOME=/opt/spark
 export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
 start-master.sh -h localhost
-start-worker.sh spark://localhost:7077
+start-worker.sh -h localhost spark://localhost:7077
 spark-submit --master spark://localhost:7077 target/scala-2.12/spark-vertica-connector-functional-tests-assembly-$CONNECTOR_VERSION.jar $1
