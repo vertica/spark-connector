73b95d3df60d35147a3500126161e0e658d71540
Alex Le <35547559+Aryex@users.noreply.github.com>
2022-05-19 13:53:56 -0700

    edited default config script.

--- functional-tests/default-config.sh
+++ functional-tests/default-config.sh
@@ -1,10 +1,9 @@
-echo -e 'functional-tests={
+echo -e 'functional-tests {
   host="'"vertica"'"
-  port="'"5433"'"
+  port=5433
   db="'"docker"'"
   user="'"dbadmin"'"
   password="'""'"
-  log='true'
   filepath="'"webhdfs://hdfs:50070/data/"'"
   truststorepath="'"/truststore.jks"'"
   truststorepassword="'"dbadmin"'"
