1df12ab8bd9fbdf8aa4c345498741c1ba6e32036
Alex Le <35547559+Aryex@users.noreply.github.com>
2022-05-31 17:21:43 -0700

    comments added to VerticaTable

--- connector/src/main/scala/com/vertica/spark/util/query/VerticaTable.scala
+++ connector/src/main/scala/com/vertica/spark/util/query/VerticaTable.scala
@@ -22,14 +22,40 @@ import cats.implicits._
 import cats.data.NonEmptyList
 import com.vertica.spark.util.error.{ConnectorError, ErrorList, IntrospectionResultEmpty, MultipleIntrospectionResult, ResultSetError}
 
+/**
+ * Abstract class containing base implementation for querying data from Vertica tables. Subclasses are expected to
+ * implement public methods that provides queries to a table. They will also need to specify a type representing
+ * a row of data.
+ *
+ * @param jdbc A jdbc layer used to query to Vertica
+ * @tparam T A concrete type representing a row of data from the table.
+ * */
 abstract class VerticaTable[T](jdbc: JdbcLayerInterface) {
 
+  /**
+   * @return The value to be used in the FROM clause of the query.
+   * */
   protected def tableName: String
 
+  /**
+   * @return A list of columns to used in the SELECT clause fo the query
+   * */
   protected def columns: Seq[String]
 
+  /**
+   * When a query contains some data, this function is called to construct row data object until
+   * the result set iterator is false.
+   *
+   * Try catch block are not needed as exceptions are caught above this function.
+   *
+   * @param rs a result set containing the next row of data.
+   * @return the object representing a row data.
+   * */
   protected def buildRow(rs: ResultSet): T
 
+  /**
+   * Make a query using parameters from concrete classes.
+   * */
   private final def _selectWhere(conditions: String): (ConnectorResult[Seq[T]], String) = {
     val cols = columns.map(wrapQuotation).mkString(", ")
     val tableName = wrapQuotation(this.tableName)
@@ -50,10 +76,22 @@ abstract class VerticaTable[T](jdbc: JdbcLayerInterface) {
     (result, query)
   }
 
+  /**
+   * Available for sub classes to make a query expecting some results.
+   *
+   * @param conditions the predicates string to be used in the WHERE clause of the query
+   * @return Either a list of row data or error
+   * */
   protected final def selectWhere(conditions: String): ConnectorResult[Seq[T]] = {
     _selectWhere(conditions)._1
   }
 
+  /**
+   * Available for sub classes to make a query expecting only one result. Will error otherwise.
+   *
+   * @param conditions the predicates string to be used in the WHERE clause of the query
+   * @return Either a single row data or error
+   * */
   protected final def selectWhereExpectOne(conditions: String): Either[ConnectorError, T] = {
     val (result, query) = _selectWhere(conditions)
      result match {
