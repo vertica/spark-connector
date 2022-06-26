b10e575b51676c1afb23618bdfcffa37cda95c52
Alex Le <35547559+Aryex@users.noreply.github.com>
2022-06-25 17:45:40 -0700

    added new complex type main example.

---
+++ examples/complex-type-examples/src/main/scala/example/Main.scala
@@ -0,0 +1,69 @@
+// (c) Copyright [2020-2021] Micro Focus or one of its affiliates.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// You may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package example
+
+import com.typesafe.config.{Config, ConfigFactory}
+import org.apache.spark.sql.{Row, SaveMode, SparkSession}
+import org.apache.spark.sql.types.{ArrayType, IntegerType, StructField, StructType}
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val conf: Config = ConfigFactory.load()
+    // Configuration options for the connector
+    val options = Map(
+      "host" -> conf.getString("functional-tests.host"),
+      "user" -> conf.getString("functional-tests.user"),
+      "db" -> conf.getString("functional-tests.db"),
+      "staging_fs_url" -> conf.getString("functional-tests.filepath"),
+      "password" -> conf.getString("functional-tests.password")
+    )
+
+    val spark = SparkSession.builder()
+      .master("local[*]")
+      .appName("Vertica Connector Test Prototype")
+      .getOrCreate()
+
+    val VERTICA_SOURCE = "com.vertica.spark.datasource.VerticaSource"
+
+    {
+      /********************************
+       *
+       * Array Examples
+       *
+       ********************************/
+      val tableName = "1D_array_test"
+      // Define schema of a table with a 1D array column
+      val schema = new StructType(Array(StructField("col1", ArrayType(IntegerType))))
+      // data
+      val data = Seq(Row(Array(1, 1, 1, 2, 2, 2)))
+      // Create a dataframe corresponding to the schema and data specified above
+      val df = spark.createDataFrame(spark.sparkContext.parallelize(data), schema).coalesce(1)
+      // Write dataframe to Vertica. note the data source
+      val writeOpts = options + ("table" -> tableName)
+      df.write.format("com.vertica.spark.datasource.VerticaSource")
+        .options(writeOpts)
+        .mode(SaveMode.Overwrite)
+        .save()
+
+      spark.read.format(VERTICA_SOURCE)
+    }
+
+    {
+
+    }
+
+  }
+
+}
