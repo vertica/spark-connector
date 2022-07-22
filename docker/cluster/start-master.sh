ea2aaaf2d9cf283046e9bac904099c677eefce0a
Alex Le <35547559+Aryex@users.noreply.github.com>
2022-07-22 09:34:28 -0700

    updated docker-compose.yml

--- docker/cluster/start-master.sh
+++ docker/cluster/start-master.sh
@@ -1,4 +1,3 @@
-CONNECTOR_VERSION=$(cat ../../version.properties | grep ${connector-version} | cut -d'=' -f2)
 export JAVA_HOME=/usr/lib/jvm/jre-11-openjdk
 export SPARK_DIST_CLASSPATH=$(/hadoop-3.3.0/bin/hadoop classpath)
 export SPARK_HOME=/opt/spark
