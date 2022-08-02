70b6684f9d2b1eab2d22fc9de6b25ce222659a44
Alex Le <35547559+Aryex@users.noreply.github.com>
2022-08-02 10:48:00 -0700

    untracked s3 example

--- examples/run-example.sh
+++
@@ -1,8 +0,0 @@
-CONNECTOR_VERSION=$(cat ../version.properties | grep ${connector-version} | cut -d'=' -f2)
-export JAVA_HOME=/usr/lib/jvm/jre-11-openjdk
-export SPARK_DIST_CLASSPATH=$(/hadoop-3.3.0/bin/hadoop classpath)
-export SPARK_HOME=/opt/spark
-export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
-start-master.sh -h localhost
-start-worker.sh spark://localhost:7077
-spark-submit --master spark://localhost:7077 $1/target/scala-2.12/spark-vertica-connector-$1-assembly-$CONNECTOR_VERSION.jar $1
